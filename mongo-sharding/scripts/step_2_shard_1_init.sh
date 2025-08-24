0a11ff77faf0e9fd11df227c5864828b4ebda4a4
evgeniia <evsi92@yandex.com>
2025-08-24 15:49:24 -0400

    created task 2 again

---
+++ mongo-sharding/scripts/step_2_shard_1_init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker exec -it shard1 mongosh --port 27018 --eval '
+rs.initiate(
+    {
+      _id : "shard1",
+      members: [
+        { _id : 0, host : "shard1:27018" }
+      ]
+    }
+)
+'
+
