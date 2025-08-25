8a0fbce0c996603dda5538aac5d484344ebdf95b
evgeniia <evsi92@yandex.com>
2025-08-25 00:57:06 -0400

    Task 1

--- mongo-sharding-repl/scripts/step_6_validation.sh
+++ mongo-sharding-repl/scripts/step_6_validation.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "mongos_router:"
+echo "\nRouter:"
 docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
 use somedb
 db.helloDoc.countDocuments()
 EOF
 
-echo -e "\nshard1:"
+echo -e "\nShard1:"
 docker compose exec -T shard11 mongosh --port 27018 --quiet <<EOF
 use somedb
 db.helloDoc.countDocuments()
 rs.status()
 EOF
 
-echo -e "\nshard2:"
+echo -e "\nShard2:"
 docker compose exec -T shard21 mongosh --port 27019 --quiet <<EOF
 use somedb
 db.helloDoc.countDocuments()
